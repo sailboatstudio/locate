c67a4e04dede8ff8b1420fe8f505b2528c171fd4
Jeremy Becker <iamjbecker@Philbert.local>
2017-09-16 04:44:22 -0700

    Update db schema, fixed jwt file typo.

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916112917) do
+ActiveRecord::Schema.define(version: 20170916113215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tokens", force: :cascade do |t|
+    t.string "token"
+    t.string "token_type"
+    t.boolean "active"
+    t.string "owner_type"
+    t.integer "owner_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
